79dd97d09f8de90f4dc664151c6b5763611efa0c
Tristan Strange <tristan.strange@gmail.com>
2015-12-10 16:52:37 +0000

    Fixed clojars name.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject music "0.1.0-SNAPSHOT"
+(defproject org.clojars.triss/music "0.1.0-SNAPSHOT"
   :description "A Clojure(Script) library with functions and data representing
                general musical knowledge, like scales, chords, intervals, etc."  
   :url "https://github.com/triss/overtone-music"
